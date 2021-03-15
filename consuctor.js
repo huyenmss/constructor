256ff02443d530dc1df3c3b1638860cf4b53cfe6
huyen_mss <huyen@mss.meidensha.co.jp>
2021-03-15 13:03:47 +0900

    new file

---
+++ consuctor.js
@@ -0,0 +1,28 @@
+function person(first, last, age){
+  this.firstname = first;
+  this.lastname = last;
+  this.age = age;
+  this.fullName = function(){
+    return this.firstname + " " + this.lastname;
+  }
+}
+
+var p1 = new  person(nguyen, huyen, 20);
+var p2 = new person(tran, hong, 25);
+
+class Student{
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+}
+
+function countDown(num){
+  if(num>0){
+    console.log(num);
+    return countDown(num-1);
+  }
+  return num;
+}
+
+countDown(10);
